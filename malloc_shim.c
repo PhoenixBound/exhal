e3c00fbf09253bc3aec5b627457f252b52fb6019
PhoenixBound <phoenixbound.github@gmail.com>
2025-06-20 00:05:56 -0500

    Commit test driver code from the other branch

---
+++ malloc_shim.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "malloc_shim.h"
+#undef malloc
+#undef calloc
+
+int allocations_so_far;
+int allocation_to_fail;
+
+void exhal_shim_init(int atf) {
+	allocations_so_far = 0;
+	allocation_to_fail = atf;
+}
+
+void *exhal_shim_malloc(size_t size) {
+	if (allocations_so_far++ == allocation_to_fail) return NULL;
+	return malloc(size);
+}
+
+void *exhal_shim_calloc(size_t num, size_t size) {
+	if (allocations_so_far++ == allocation_to_fail) return NULL;
+	return calloc(num, size);
+}
